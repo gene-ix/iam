df2d1210bc5e84c40c629b2b0bfdcf3283e69d05
gene-ix <110238523+gene-ix@users.noreply.github.com>
2023-04-07 21:04:33 +0900

    gene-ix

--- src/js/util.js
+++
@@ -1,7 +0,0 @@
-export const getJson = (url) => {
-    return new Promise((resolve, reject) => {
-        fetch(url)
-            .then((res) => res.json())
-            .then((data) => resolve(data));
-    });
-};
